d506552ed2ebb68fe7c466702b46957a06fc378d
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2015-02-26 16:56:37 +0100

    Default language in routing.

--- config/routes.php
+++ config/routes.php
@@ -2,5 +2,5 @@
     // specific publication routing rules
 
     // DO NOT EDIT OR CHANGE BELOW!!
-    Router::connect('/lang/:lang/*', array('controller' => 'pages', 'action' => 'route'), array('lang' => '[a-z]{2,3}', 'persist' => array('lang')));
+    Router::connect('/lang/:lang/*', array('controller' => 'pages', 'action' => 'route', 'lang' => 'eng'), array('lang' => '[a-z]{2,3}', 'persist' => array('lang')));
     Router::connect('/*', array('controller' => 'pages', 'action' => 'route'));
